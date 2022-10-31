eb630b2b56b724a0d95f8565f1c67ca7599144b4
ChoNg-YuN? <107825620+lluhfii@users.noreply.github.com>
2022-10-31 17:09:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//mengubah variable teks menjadi uppercase
+const nama = "Merah Putih"
+nama.toUpperCase()
+
+// mengubah variable teks menjadi lowercase
+const nama = "Merah Putih"
+nama.toLowerCase()
+
+//Mengganti nama dalam Variable ( Putih ) to ( Muda )
+const nama = "Merah Putih"
+console.log(str.replace('Putih', 'Muda'))
+
+//Mengukur panjang dari Variable
+const str = "Merah Putih"
+console.log(str.length)
